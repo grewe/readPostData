0894b650a63464ac1b2f75ddffa2578287226342
Lynne Grewe <lynne.grewe@csueastbay.edu>
2017-11-01 14:51:31 -0700

    adding reading of data sent in request

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+//########################################
+//to process data sent in on request need body-parser module
+var bodyParser = require('body-parser');
+var path = require ('path'); //to work with separtors on any OS including Windows
+
+router.use(bodyParser.json()); // for parsing application/json
+
+router.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+//#########################################
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -9,12 +19,22 @@ router.get('/', function(req, res, next) {
 /* the URI /readNameAndRespond */
 
 router.get('/readNameAndRespond', function(req, res, next) {
-    res.send("hello");
+    //expecting data variable called name --retrieve value using body-parser
+    var body = JSON.stringify(req.body);  //if wanted entire body as JSON
+    var params = JSON.stringify(req.params);//if wanted parameters
+    var name = req.body.name;  //retrieve the data associated with name
+    var value_name = req.body.say;
+    res.send("hello" + value_name);
 });
-
 //now processing post
 router.post('/readNameAndRespond', function(req, res, next) {
-    res.send("hello");
+
+    //expecting data variable called name --retrieve value using body-parser
+    var body = JSON.stringify(req.body);  //if wanted entire body as JSON
+    var params = JSON.stringify(req.params);//if wanted parameters
+    var name = req.body.name;  //retrieve the data associated with name
+    var value_name = req.body.say;
+    res.send("hello" + value_name);
 });
 
 module.exports = router;
